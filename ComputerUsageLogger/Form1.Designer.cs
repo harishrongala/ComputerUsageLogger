15e0b661992f2a46045ddda8ca5d106e30966ed5
Harish Kumar Rongala <harishrongala@gmail.com>
2018-01-04 19:55:49 -0500

    solution files

---
+++ ComputerUsageLogger/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace ComputerUsageLogger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "UsageLogger";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(103, 28);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(102, 24);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(258, 71);
+            this.Name = "Form1";
+            this.Text = "Logger";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ ComputerUsageLogger/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+
+namespace ComputerUsageLogger
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, out uint ProcessId);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        StreamWriter sw;
+        Thread threadForLogging;
+
+        public Form1()
+        {
+            InitializeComponent();
+            String filePath = @"C:\Users\haris\Dropbox\computerUsageLogs\" + DateTime.Today.ToLongDateString() + ".txt";
+            if (!File.Exists(filePath))
+            {
+                sw = File.CreateText(filePath);
+            }
+            else
+            {
+                sw = new StreamWriter(File.Open(filePath, FileMode.Append));
+            }
+
+            sw.AutoFlush = true;
+
+            sw.WriteLine("Computer Started at " + DateTime.Now.ToLongTimeString());
+
+            threadForLogging = new Thread(keepLogging);
+            threadForLogging.Start();
+
+           
+
+        }
+
+        public void keepLogging()
+        {
+            while (true)
+            {
+                IntPtr win = GetForegroundWindow();
+                uint pid;
+                GetWindowThreadProcessId(win, out pid);
+                Process p = Process.GetProcessById((int)pid);
+                String ProcessName = p.ProcessName.ToString();
+                String WindowTitle = p.MainWindowTitle.ToString();
+                String timeStamp = DateTime.Now.ToLongTimeString();
+                ProcessName.Replace('#', '-');
+                WindowTitle.Replace('#', '-');
+                timeStamp.Replace('#', '-');
+                // Field Seperator is #
+                //Console.WriteLine(ProcessName + " # " + WindowTitle + " # " + timeStamp);
+                sw.WriteLine(ProcessName + " # " + WindowTitle + " # " + timeStamp);
+                // Log Every minute
+                Thread.Sleep(60000);
+
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = false;
+            this.Opacity = 0.0f;
+            this.WindowState = FormWindowState.Minimized;
+            
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            threadForLogging.Abort();
+            sw.Close();
+            Application.Exit();
+        }
+    }
+}
